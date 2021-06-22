be246042f0d733b0e33ee432349432409dd53009
Kisaragi <budzhalov99@gmail.com>
2021-06-22 23:12:31 +0300

    some progress

--- src/components/RecipientsDisplay.svelte
+++ src/components/RecipientsDisplay.svelte
@@ -1,15 +1,46 @@
 <script lang="ts">
   export let recipients:Array<string>
 
-
-
-
-
-
   const recipientsResult:Array<string> = recipients; // replace recipients with logic that they want. 
-  console.log(recipients);
+
 </script>
 
+<style>
+  .box{
+    color: white;
+    padding: 2px 5px;
+    font-size: 16px;
+    background-color: #666666;
+    border-radius: 3px;
+    margin-left:5px ;
+  }
+  .wrapper{
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .recipientEmail{
+  }
+</style>
+<!-- Если к нам пришел массив с длиной больше 1, то надо показать компонент (квадратик с цифрой)  -->
+<!-- Если же в ячейку могут поместиться все элементы, то надо показать всех -->
+<!-- -->
+<!-- -->
+<!-- -->
+<!-- -->
 
 
+
+{#if recipientsResult.length > 1}  
+<div class="wrapper">
+  <spa class="recipientEmail">{recipientsResult[0]}</spa>
+  <span class={"box"}>{`+${recipientsResult.length - 1}`}</span>
+</div>
+
+
+{:else}
 <span>{recipientsResult}</span>
+{/if}
+
+
+
