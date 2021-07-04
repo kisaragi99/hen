c6d4084f0e7726dbb93d634f685ffccaf90dc258
kisaragi99 <58854608+kisaragi99@users.noreply.github.com>
2021-07-04 23:34:11 +0300

    Create api.svelte

---
+++ api.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import axios from 'axios'
+
+  function sendReq() {
+    const hennegeApi = axios.create({
+      baseURL: "https://cors-anywhere.herokuapp.com/https://api.challenge.hennge.com/challenges/003",
+      headers: {
+        "Authorization": "Basic YnVkemhhbG92OTlAZ21haWwuY29tOkhFTk5HRUNIQUxMRU5HRQ==",
+        "Accept": "*/*",
+        "Host": "api.challenge.hennge.com",
+        "Content-Type": "application/json"
+      },
+    })
+
+    hennegeApi
+      .post('', {
+        'github_url':'https://gist.github.com/kisaragi99/7d627f6e949aa2df73e1ad9c41dfe019',
+        'contact_email': 'budzhalov99@gmail.com'
+      })
+      .then(responce => console.log(responce))
+      .catch(err => {
+        console.log(err.message)
+      })
+  }
+
+</script>
+
+<style lang="scss">
+
+</style>
+
+<button
+  on:click={() => {
+    sendReq();
+  }}>
+  send
+</button>
